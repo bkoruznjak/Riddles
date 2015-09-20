bd912cd9ad02fecb8704769d366151c1c750a3aa
bkoruznjak <borna.koruznjak@gmail.com>
2015-09-20 16:42:30 +0200

    initial commit

---
+++ app/src/main/java/hr/from/bkoruznjak/myfirstandroidapp/SplashScreen.java
@@ -0,0 +1,107 @@
+package hr.from.bkoruznjak.myfirstandroidapp;
+
+/**
+ * Created by borna on 19.09.15..
+ */
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+import hr.from.bkoruznjak.myfirstandroidapp.db.DatabaseHandler;
+import hr.from.bkoruznjak.myfirstandroidapp.db.Riddle;
+
+public class SplashScreen extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+    private static final String TAG = "splash_activity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                Log.v(TAG, "run method start");
+
+                //init the database
+
+                DatabaseHandler dbHandler = new DatabaseHandler(SplashScreen.this);
+                Log.d("dbHandler: ", "createdDbHandler ..");
+
+                try {
+                    //get resources file
+                    Resources res = getResources();
+                    InputStream riddleInputStream = res.openRawResource(R.raw.riddles);
+
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(riddleInputStream));
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        Log.v(TAG,line);
+                        String[] riddleData = line.split(" --- ");
+                        String riddleId = riddleData[1].replaceAll("<br/>", "\n");
+                        String riddleText = riddleData[2].replaceAll("<br/>","\n");
+                        String riddleAnwser = riddleData[3].replaceAll("<br/>","\n");
+                        Log.d(TAG + " id:",riddleId);
+                        Log.d(TAG + " text:",riddleText);
+                        Log.d(TAG + " anwser:",riddleAnwser);
+                        if (dbHandler.getRiddle(riddleId).getId() == null) {
+                            dbHandler.addRiddle(new Riddle(riddleId, riddleText, riddleAnwser, 0));
+                            Log.d(TAG, "row inserted successfully");
+                        } else {
+                            Log.d(TAG, "row already exists");
+                        }
+
+                    }
+                } catch (Exception e) {
+                    // e.printStackTrace();
+                    Log.e(TAG, "" + e);
+                }
+
+                // Reading all riddles
+                Log.d(TAG, "Reading all riddles..");
+                List<Riddle> riddleList = dbHandler.getAllRiddles();
+
+                for (Riddle riddle : riddleList) {
+                    String riddleLog = "Id: "+riddle.getId()
+                            + " ,Text: " + riddle.getRiddleText()
+                            + " ,Anwser: " + riddle.getRiddleAnwser()
+                            + " ,ViewCount: " + riddle.getViewCount();
+                    // Writing Riddles to log
+                    Log.d(TAG, riddleLog);
+                }
+
+                Log.v(TAG, "setting up the intent");
+
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                Log.v(TAG, "starting activity");
+                startActivity(i);
+                Log.v(TAG, "calling finish");
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
---
+++ app/src/main/java/hr/from/bkoruznjak/myfirstandroidapp/db/Riddle.java
@@ -0,0 +1,57 @@
+package hr.from.bkoruznjak.myfirstandroidapp.db;
+
+import java.io.Serializable;
+
+/**
+ * Created by borna on 20.09.15..
+ */
+public class Riddle implements Serializable {
+
+    private String id;
+    private String riddleText;
+    private String riddleAnwser;
+    private int viewCount;
+
+    public Riddle(){
+
+    }
+
+    public Riddle(String id, String riddleText, String riddleAnwser, int viewCount){
+        this.id = id;
+        this.riddleText = riddleText;
+        this.riddleAnwser = riddleAnwser;
+        this.viewCount = viewCount;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRiddleText() {
+        return riddleText;
+    }
+
+    public void setRiddleText(String riddleText) {
+        this.riddleText = riddleText;
+    }
+
+    public String getRiddleAnwser() {
+        return riddleAnwser;
+    }
+
+    public void setRiddleAnwser(String riddleAnwser) {
+        this.riddleAnwser = riddleAnwser;
+    }
+
+    public int getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(int viewCount) {
+        this.viewCount = viewCount;
+    }
+}
