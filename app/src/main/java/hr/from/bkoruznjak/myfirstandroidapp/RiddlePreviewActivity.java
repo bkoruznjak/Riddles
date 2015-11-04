ea3fc664291bb31db03b61d54a2094712ee7d854
bkoruznjak <borna.koruznjak@gmail.com>
2015-11-04 21:57:20 +0100

    - i rekt everything

---
+++ app/src/main/java/hr/from/bkoruznjak/myfirstandroidapp/RiddlePreviewActivity.java
@@ -0,0 +1,113 @@
+package hr.from.bkoruznjak.myfirstandroidapp;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v13.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+import hr.from.bkoruznjak.myfirstandroidapp.db.Riddle;
+
+public class RiddlePreviewActivity extends FragmentActivity {
+    private static int NUM_PAGES = 5;
+
+    private ArrayList<Riddle> riddleArrayList = new ArrayList<>();
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_riddle_preview);
+
+        //set dummy riddle array data
+        for (int i = 0; i < 10 ; i++) {
+            riddleArrayList.add(i, new Riddle("" + i, "riddle text for riddle number:" + i, "riddle anwser for riddle number:" + i, 0, 0));
+        }
+        NUM_PAGES = riddleArrayList.size();
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+        mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                // When changing pages, reset the action bar actions since they are dependent
+                // on which page is currently active. An alternative approach is to have each
+                // fragment expose actions itself (rather than the activity exposing actions),
+                // but for simplicity, the activity provides the actions in this sample.
+                invalidateOptionsMenu();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.activity_riddle_preview, menu);
+
+        menu.findItem(R.id.action_previous).setEnabled(mPager.getCurrentItem() > 0);
+
+        // Add either a "next" or "finish" button to the action bar, depending on which page
+        // is currently selected.
+        MenuItem item = menu.add(Menu.NONE, R.id.action_next, Menu.NONE,
+                (mPager.getCurrentItem() == mPagerAdapter.getCount() - 1)
+                        ? R.string.action_finish
+                        : R.string.action_next);
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // Navigate "up" the demo structure to the launchpad activity.
+                // See http://developer.android.com/design/patterns/navigation.html for more.
+                NavUtils.navigateUpTo(this, new Intent(this, MainActivity.class));
+                return true;
+
+            case R.id.action_previous:
+                // Go to the previous step in the wizard. If there is no previous step,
+                // setCurrentItem will do nothing.
+                Log.d("test", "action prev");
+                mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+                return true;
+
+            case R.id.action_next:
+                // Advance to the next step in the wizard. If there is no next step, setCurrentItem
+                // will do nothing.
+                Log.d("test", "action next");
+                mPager.setCurrentItem(mPager.getCurrentItem() + 1);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return RiddleSlidePageFragment.create(position, riddleArrayList.get(position));
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+}
---
+++ app/src/main/java/hr/from/bkoruznjak/myfirstandroidapp/RiddleSlidePageFragment.java
@@ -0,0 +1,71 @@
+package hr.from.bkoruznjak.myfirstandroidapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import hr.from.bkoruznjak.myfirstandroidapp.db.Riddle;
+
+public class RiddleSlidePageFragment extends Fragment {
+
+    public static final String ARG_PAGE = "page";
+    public static final String ARG_RIDDLE = "riddle";
+
+    private int mPageNumber;
+    private String riddleId;
+    private String riddleText;
+    private String riddleAnwser;
+
+    public static RiddleSlidePageFragment create(int pageNumber, Riddle riddle) {
+        RiddleSlidePageFragment fragment = new RiddleSlidePageFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, pageNumber);
+        args.putSerializable(ARG_RIDDLE, riddle);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public RiddleSlidePageFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPageNumber = getArguments().getInt(ARG_PAGE);
+        Riddle argRiddle = (Riddle)getArguments().getSerializable(ARG_RIDDLE);
+        this.riddleId = argRiddle.getId();
+        this.riddleText = argRiddle.getRiddleText();
+        this.riddleAnwser = argRiddle.getRiddleAnwser();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout containing a title and body text.
+        ViewGroup rootView = (ViewGroup) inflater
+                .inflate(R.layout.fragment_riddle, container, false);
+
+        // Set the title view to show the page number.
+        ((TextView) rootView.findViewById(android.R.id.text1)).setText(
+                getString(R.string.page_number, mPageNumber + 1));
+
+        // Set the id view to show the page number.
+        ((TextView) rootView.findViewById(R.id.riddleId1)).setText(riddleId + 1);
+
+        // Set the riddle Text view to show the page number.
+        ((TextView) rootView.findViewById(R.id.riddleText1)).setText(riddleText);
+
+        // Set the riddle Anwser view to show the page number.
+        ((TextView) rootView.findViewById(R.id.riddleAnwser1)).setText(riddleAnwser);
+
+        return rootView;
+    }
+
+    public int getPageNumber() {
+        return mPageNumber;
+    }
+}
